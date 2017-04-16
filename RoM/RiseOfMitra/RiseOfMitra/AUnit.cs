e0eb2eef7f559ae230d804350f6684c5d3ffdafd
Thales Aguiar <thalesaguiar21@gmail.com>
2017-04-16 08:08:33 -0300

    Added Pawn.cs and RoMUtils.cs

--- RoM/RiseOfMitra/RiseOfMitra/AUnit.cs
+++ RoM/RiseOfMitra/RiseOfMitra/AUnit.cs
@@ -4,10 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using RoMUtils;
+
 namespace RiseOfMitra
 {
-    abstract class Unit
+    abstract class AUnit
     {
-
+        protected Pair Position;
+        protected int LifePoints;
     }
 }
--- RoM/RiseOfMitra/RiseOfMitra/IControlable.cs
+++ RoM/RiseOfMitra/RiseOfMitra/IControlable.cs
@@ -8,5 +8,6 @@ namespace RiseOfMitra
 {
     interface IControlable
     {
+        
     }
 }
