045b9ef607223161d4afefb3ed06cb3d6e392ca0
Thales Aguiar <thalesaguiar21@gmail.com>
2017-07-11 17:47:29 -0300

    renamed classes to be correct with the standards

---
+++ RiseOfMitra/MonteCarlo/ISelectionStrategy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RiseOfMitra.Players.Commands;
+
+namespace RiseOfMitra.MonteCarlo
+{
+    interface ISelectionStrategy
+    {
+        Node Execute();
+    }
+}
---
+++ RiseOfMitra/MonteCarlo/ISimulationStrategy.cs
@@ -0,0 +1,14 @@
+﻿using RiseOfMitra.Players.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiseOfMitra.MonteCarlo
+{
+    interface ISimulationStrategy
+    {
+        List<ACommand> Execute();
+    }
+}
