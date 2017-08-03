ce05d0f7178f50ee6f4a4fef062c492fedb89f6b
Thales Aguiar <thalesaguiar21@gmail.com>
2017-08-03 20:48:02 -0300

    Added Exapasion strategy

---
+++ RiseOfMitra/MonteCarlo/ExpandAll.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiseOfMitra.MonteCarlo
+{
+    public class ExpandAll : IExpansion
+    {
+        public void Expand(Node root, Node subTree) {
+            if (root != null) {
+                if (!root.Childs.Contains(subTree)) {
+                    root.Childs.Add(subTree);
+                    subTree.VisitCount++;
+                } else {
+                    int i = root.Childs.IndexOf(subTree);
+                    root.Childs[i].VisitCount++;
+                }
+            }
+        }
+    }
+}
